2077706a62e2ea750cbc5ccfa4740dd27e2641fe
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-02-16 23:55:17 +0600

    updated practice2 file

--- practice2.js
+++ practice2.js
@@ -11,4 +11,63 @@ function checkDigit(str){
 const str = '*&@';
 
 const ans = checkDigit(str);
-console.log(ans);
+// console.log(ans);
+
+
+
+
+
+
+
+
+
+function calculateFinalScore(student){
+    // whelther input is object or not
+    if(typeof student !== 'object') return 'Invalid Input';
+
+    // if marks >= 80 true otherwise false
+    let totalMarks = student.testScore + student.schoolGrade;
+    if(student.isFFamily === true) totalMarks += 20;
+
+    if(totalMarks >= 80) return true;
+    else return false;
+}
+
+const student = {
+    name:'Rajib',
+    testScore:15,
+    schoolGrade:25,
+    isFFamily:true,
+}
+
+const output = calculateFinalScore(student);
+// console.log(output);
+
+
+
+
+
+
+
+
+function waitingTime(waitingTimes, serialNumber){
+    // invalid input
+    if(!Array.isArray(waitingTimes) || typeof serialNumber !== 'number') return 'Invalid Input';
+
+    let sum = 0;
+    const n = waitingTimes.length;
+    for(let i of waitingTimes){
+        sum += i;
+    }
+
+    const avg = parseInt(sum / n);
+    const remaingNumber = (serialNumber - 1) - n;
+    return avg * remaingNumber;
+
+}
+
+const waitingTimes = [6,2,5,2,8];
+const serialNumber = 6;
+
+const timeRequired = waitingTime(waitingTimes, serialNumber);
+console.log(timeRequired);
