4d2daee15dd32f775b9624a6f198bc5523d765b4
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-02-18 14:36:54 +0600

    added assignment file

--- assignment.js
+++ assignment.js
@@ -34,4 +34,44 @@ function  willSuccess( marks ) {
     else return false;
 }
 
+function  validProposal( person1 , person2 ) {
 
+    if(typeof person1 !== 'object' || typeof person2 !== 'object') return 'Invalid';
+
+    let flag = false;
+    if((person1.gender === 'female' && person2.gender === 'male')||
+    (person1.gender === 'male' && person2.gender === 'female')){
+        flag = true;
+    }
+
+    let ageDifference = Math.abs(person1.age - person2.age);
+    if(flag === true && ageDifference <= 7) return true;
+    else return false;
+
+}
+
+function  calculateSleepTime( times ) {
+    
+    let sum = 0;
+    for(let i = 0; i < times.length; i++){
+        if(typeof times[i] !== 'number') return 'Invalid';
+        sum += times[i];
+    }
+
+    let hr = 0, min = 0, sec = 0;
+
+    if(sum >= 3600){
+        hr = parseInt(sum / 3600);
+        sum -= (hr * 3600);
+    }
+
+    if(sum < 60) sec = sum;
+
+    else if(sum >= 60){
+        min = parseInt(sum / 60);
+        sum -= (min * 60);
+    }
+    sec = sum;
+
+    return '{ hour:'+ hr + ',' + 'minute:'+ min + ','+ 'second:'+ sec + ' }';
+}
